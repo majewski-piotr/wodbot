a800a00d33c5bcf4d222cc56bd6737a151740580
John Doe <majewski.piotr.511@gmail.com>
2021-10-31 23:19:43 +0100

    restructured consumers, created helper class, added one roll option and help

--- src/main/java/com/wodbot/WorldOfDarknessBOT.java
+++ src/main/java/com/wodbot/WorldOfDarknessBOT.java
@@ -5,9 +5,12 @@ import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 
-import com.wodbot.eventhandlers.MessageHandler;
+import com.wodbot.eventhandlers.messageEventHandlers.MessageHandler;
+import org.apache.log4j.Logger;
 
 public class WorldOfDarknessBOT {
+
+    private static final Logger log = Logger.getLogger(WorldOfDarknessBOT.class);
 
     public static void main(final String[] args) {
         final DiscordClient client = DiscordClient.create(System.getenv("DISCORD_TOKEN"));
@@ -15,12 +18,12 @@ public class WorldOfDarknessBOT {
 
         gateway.on(MessageCreateEvent.class).subscribe(event -> {
             final Message message = event.getMessage();
-            for(MessageHandler handler : MessageHandler.values()){
-                if(handler.matches(message)){
+            for (MessageHandler handler : MessageHandler.values()) {
+                if (handler.matches(message)) {
                     handler.accept(message);
                 }
             }
         });
-            gateway.onDisconnect().block();
+        gateway.onDisconnect().block();
     }
 }
