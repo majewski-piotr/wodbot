2843ba8ecc3fdbe63a8bbab3ad084deddd6a560b
majewski-piotr <majewski.piotr.511@gmail.com>
2022-01-07 11:28:02 +0100

    Completed simple registrations process

--- src/main/java/com/piotrm/wodbot/service/UserService.java
+++ src/main/java/com/piotrm/wodbot/service/UserService.java
@@ -3,6 +3,7 @@ package com.piotrm.wodbot.service;
 import com.piotrm.wodbot.dao.UserRepository;
 import com.piotrm.wodbot.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +12,14 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public boolean saveNewUser(String email, String username, String password){
         if(!userRepository.findUserByEmail(email).isPresent()){
             User user = new User();
             user.setUsername(username);
-            user.setPassword(password);
+            user.setPassword(passwordEncoder.encode(password));
             user.setEmail(email);
             user.setLanguage("ENG");
             userRepository.save(user);
