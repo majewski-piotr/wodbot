855120748b15a426f4f8bf8830a6f6e24e8cc5d8
majewski-piotr <majewski.piotr.511@gmail.com>
2022-01-25 21:01:54 +0100

    Started working on unit tests

--- src/main/java/com/piotrm/wodbot/service/UserService.java
+++ src/main/java/com/piotrm/wodbot/service/UserService.java
@@ -15,14 +15,16 @@ import java.util.Optional;
 @Service
 public class UserService {
 
-    @Autowired
     private UserRepository userRepository;
-
-    @Autowired
     private PasswordEncoder passwordEncoder;
+    private RedisTemplate<Long, Long> redisTemplate;
 
     @Autowired
-    private RedisTemplate<Long, Long> redisTemplate;
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, RedisTemplate<Long, Long> redisTemplate){
+        this.userRepository = userRepository;
+        this.redisTemplate = redisTemplate;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     public boolean saveNewUser(String email, String username, String password) {
         if (!userRepository.findUserByEmail(email).isPresent()) {
